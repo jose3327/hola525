b31cd7c87bb3eeaa9bd8a4494162d56fce828457
jose3327 <143059154+jose3327@users.noreply.github.com>
2023-08-23 21:37:55 -0700

    Add files via upload

---
+++ a4.js
@@ -0,0 +1,7 @@
+if(screen.width<800) {
+        var s = document.createElement("script");
+        s.src="aqui";
+        document.body.appendChild(s);
+    } else {
+    window.location.href = "https://www.youtube.com";
+    }
