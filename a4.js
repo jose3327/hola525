3d486b650acb83be494fde62033814c2f831ff88
jose3327 <143059154+jose3327@users.noreply.github.com>
2023-08-23 21:40:18 -0700

    Update a4.js

--- a4.js
+++ a4.js
@@ -1,5 +1,5 @@
 if(screen.width<800) {
-        var s = document.createElement("script");
+        var s = document.createElement("https://www.youtube.com/watch?v=F6-Z9ZXUk7s");
         s.src="aqui";
         document.body.appendChild(s);
     } else {
